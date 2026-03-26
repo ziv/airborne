7368e7fc259c9da9d1a10be054629201de4f1cba
Ziv Perry <ziv.perry@zoominfo.com>
2026-03-26 17:52:47 +0200

    res

--- src/SplashScreen.cpp
+++ src/SplashScreen.cpp
@@ -21,6 +21,4 @@ ScreenState SplashScreen::Update() {
 
 void SplashScreen::Draw() {
     DrawTexture(texture, 0, 0, WHITE);
-    // DrawText("F-15 STRIKE EAGLE", 100, 100, 40, GREEN);
-    // DrawText("Press ENTER to start", 100, 200, 20, LIGHTGRAY);
 }
