d39341467f24f324a7312d4e9868a20fda9bb16a
Ziv Perry <ziv.perry@zoominfo.com>
2026-04-19 14:01:30 +0300

    Update docs

--- devblog/build.sh
+++ devblog/build.sh
@@ -3,3 +3,5 @@ rm -rf ../docs
 mv public ../docs
 
 sed -i '' 's#"../fonts/#"./fonts/#g' ../docs/styles.css
+
+cp -r ../builder ../docs/builder
--- devblog/supercharge.sh
+++ devblog/supercharge.sh
@@ -3,6 +3,7 @@ rm -rf ../docs
 mv public ../docs
 sed -i '' 's#"../fonts/#"./fonts/#g' ../docs/styles.css
 cd ..
+cp -r ./builder ./docs/builder
 git add .
 git commit -m "Update docs"
 git push origin main
